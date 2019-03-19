94dd4b3535878685c5fae804dab15af9e593f367
James Cox <jamesncox@yahoo.com>
2019-03-19 15:51:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,6 @@ def word_substituter(tweet)
   tweet.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
-      binding.pry
     else
       word
     end
